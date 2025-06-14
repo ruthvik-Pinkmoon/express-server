c930394b9514da1b93c9d49cd706ad3bd35ac2cd
pinkmoon-technologies <gitrepo@pinkmoontech.com>
2025-06-14 23:44:22 +0530

    updaetd code

--- api/index.js
+++ api/index.js
@@ -14,7 +14,9 @@ app.use((req, res, next) => {
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  if (req.method === "OPTIONS") return res.status(200).end();
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
   next();
 });
 
